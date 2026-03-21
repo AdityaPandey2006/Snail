71fa01319a56d20dc7c509ae11246bfc9a7504f4
AdityaPandey2006 <adipandeyside123@gmail.com>
2026-03-21 15:39:53 +0530

    sendToDump function added

--- src/fileDump.c
+++ src/fileDump.c
@@ -18,11 +18,11 @@
 int makeDumpFolder(){
     char* home=getenv("HOME");
     char dumpPath[PATH_MAX];
-    sprintf("%s/.snailDump/",home);
     char dumpFilesPath[PATH_MAX];
-    sprintf("%s/files/",dumpPath);
     char dumpInfoPath[PATH_MAX];
-    sprintf("%s/info/",dumpPath);
+    sprintf(dumpPath,"%s/.snailDump/",home);
+    sprintf(dumpFilesPath,"%s/files/",dumpPath);
+    sprintf(dumpInfoPath,"%s/info/",dumpPath);
     int success=1;
     if(mkdir(dumpPath,0755)==-1&& errno!=EEXIST){
         success=0;
@@ -36,15 +36,15 @@ int makeDumpFolder(){
     return success;
 }
 
-int clearDump(){
+int cleanDump(){
     char* home=getenv("HOME");
     char dumpPath[PATH_MAX];
     char dumpFilesPath[PATH_MAX];
     char dumpInfoPath[PATH_MAX];
 
-    sprintf("%s/.snailDump/",home);
-    sprintf("%s/files/",dumpPath);
-    sprintf("%s/info/",dumpPath);
+    sprintf(dumpPath,"%s/.snailDump/",home);
+    sprintf(dumpFilesPath,"%s/files/",dumpPath);
+    sprintf(dumpInfoPath,"%s/info/",dumpPath);
 
     int startLoop=1;
     if((access(dumpPath,F_OK)!=0)||(access(dumpFilesPath,F_OK)!=0)||(access(dumpInfoPath,F_OK)!=0)){
@@ -58,6 +58,9 @@ int clearDump(){
     }
     DIR* dumpDir=opendir(dumpPath);
     DIR* dumpFileDir=opendir(dumpFilesPath);
+    if(!dumpFileDir){
+        return 0;
+    }
     DIR* dumpInfoDir=opendir(dumpInfoPath);
     struct dirent* entry;
     int dumpCleanSuccess=1;
@@ -67,10 +70,13 @@ int clearDump(){
             continue;
         }
         strcpy(entryName,entry->d_name);
-        char entryInfoPath[PATH_MAX + 1];//the path of the entry in the info folder of the dump will be stored here. This path will be required if we wish to delete this file from the info folder during cleanup.
-        char entryFilePath[PATH_MAX + 1];//the path of the entry in the files folder of the dump will be stored here. This path will be required if we wish to delete this file from the files folder during cleanup.
+        //the path of the entry in the info folder of the dump will be stored here. This path will be required if we wish to delete this file from the info folder during cleanup.
+        char entryInfoPath[PATH_MAX + 1];
+        //the path of the entry in the files folder of the dump will be stored here. This path will be required if we wish to delete this file from the files folder during cleanup.
+        char entryFilePath[PATH_MAX + 1];
         sprintf(entryInfoPath, "%s/%s.snailInfo", dumpInfoPath, entryName);
         sprintf(entryFilePath,"%s/%s", dumpFilesPath, entryName);
+        //will read the trash info of the current entry
         FILE* entryInfoFilePointer;
         entryInfoFilePointer=fopen(entryInfoPath,"r");//we open the info file of the entry in read mode to see its deletion time
         if (entryInfoFilePointer==NULL) {
@@ -88,7 +94,7 @@ int clearDump(){
                 strcpy(deletionTimeString,buffer);
             }
             else{
-                if(strcnmp(buffer,"DeletionDate",13)==0){
+                if(strncmp(buffer,"DeletionDate:",13)==0){
                     dFound=1;
                 }
             }
@@ -119,19 +125,77 @@ int clearDump(){
 
 
 int sendToDump(char* entryPath){
+    int success=1;
     char* home=getenv("HOME");
     char dumpPath[PATH_MAX];
     char dumpFilesPath[PATH_MAX];
     char dumpInfoPath[PATH_MAX];
 
-    sprintf("%s/.snailDump/",home);
-    sprintf("%s/files/",dumpPath);
-    sprintf("%s/info/",dumpPath);
+    sprintf(dumpPath,"%s/.snailDump/",home);
+    sprintf(dumpFilesPath,"%s/files/",dumpPath);
+    sprintf(dumpInfoPath,"%s/info/",dumpPath);
+
+    //if the dump does not exist, we create it
+    if((access(dumpPath,F_OK)!=0)||(access(dumpFilesPath,F_OK)!=0)||(access(dumpInfoPath,F_OK)!=0)){
+        success=makeDumpFolder();
+        if(!success){
+            return success;
+        }
+    }
+
+    //entryPath is the current path at which the entry is present
     int maxNameLength=256;
     char entryName[maxNameLength];
     int ch='/';
-    char* lastPosOfSlash=strchr(entryPath,ch);
-    lastPosOfSlash++;
-    strcpy(entryName,lastPosOfSlash);
-    
+    char* lastPosOfSlash=strrchr(entryPath,ch);
+    if(lastPosOfSlash){
+        lastPosOfSlash++;
+    }
+    else{
+        lastPosOfSlash=entryPath;
+    }//will have to look into this. if the user tries to delete "folder" and "folder/", the behaviour might be different
+    strncpy(entryName,lastPosOfSlash,sizeof(entryName)-1);
+    entryName[sizeof(entryName)-1]='\0';
+
+    //finding deletion time and converting it to the formatted time string
+    time_t deletionTime;//the current time is the deletion time
+    time(&deletionTime);
+    struct tm* timeStorage;
+    char deletionTimeString[25];
+    timeStorage=localtime(&deletionTime);
+    char* format = "%Y-%m-%dT%H:%M:%S";
+    strftime(deletionTimeString,25,format,timeStorage);
+
+
+    //entryFilePath is the location the entry will go to in the files/ folder of the dump
+    //can later replace rename if it disturbs cross filesystems too much
+    char entryFilePath[PATH_MAX];
+    sprintf(entryFilePath,"%s%s_%ld",dumpFilesPath,entryName,deletionTime);
+    if((rename(entryPath,entryFilePath))!=0){
+        success=0;
+        return success;
+    }
+
+    //entryInfoPath is the location the entry will go to in the info/ folder of the dump
+    char entryInfoPath[PATH_MAX];
+    sprintf(entryInfoPath,"%s%s_%ld.snailInfo",dumpInfoPath,entryName,deletionTime);
+    //will write the trash info of the file into the entryInfoFile
+    FILE* entryInfoFilePointer;
+    entryInfoFilePointer=fopen(entryInfoPath,"w");
+    if(!entryInfoFilePointer){
+        printf("dumping of file %s failed",entryPath);
+        //if metadata writing fails restore the file.
+        rename(entryFilePath,entryPath);
+        success=0;
+        return success;
+    }
+    char trashInfo[3*PATH_MAX];
+    sprintf(trashInfo,"[Trash Info]\nPath:\n%s\nDeletionDate:\n%s\n",entryPath,deletionTimeString);
+    fputs(trashInfo,entryInfoFilePointer);
+    fclose(entryInfoFilePointer);
+    return success;
+}
+
+int fileRestore(){
+
 }
